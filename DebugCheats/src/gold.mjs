86ee3c3827232b709fb6924634b614ed1cc87b67
Vladulentor <mihailvladutpopescu@gmail.com>
2025-09-28 10:25:33 +0300

    Cleanup

--- DebugCheats/src/gold.mjs
+++
@@ -1,5 +0,0 @@
-const goldSettings = (await mod.getContext(import.meta).loadModule('src/goldSettings.mjs'));
-
-export function setGold(ctx) {
-    game.gp.set(goldSettings.getGoldQuantity(ctx));
-}

ef3d27f1df0978043860f8cc7191a08d9e13f374
Vladulentor <mihailvladutpopescu@gmail.com>
2025-09-29 21:10:49 +0300

    finishing touches

--- Construction/src/modPatches/skillboosts.mjs
+++ Construction/src/modPatches/skillboosts.mjs
@@ -3,15 +3,15 @@ export function skillBoostsCompatibility() {
         // Required //
         skill: game.construction,
         // Optional //
-        realmIDs: ['melvorD:Melvor'], // Just write it explicitly
+        realmIDs: ['melvorD:Melvor'], // My god Slash, you really made it easy for idiots like me, after trying to get ETA to somehow work for a day straight this is amazing
         //header: HTMLElement, // A header is only required if there is no `skill.header` property //
         noPreservation: false,
-        noMastery: true,
-        noSummon: false,
-        noPotion: false,
+        noMastery: false,
+        noSummon: true,
+        noPotion: true,
         noDoubling: false,
         noInterval: false,
-        noConsumable: false,
+        noConsumable: true,
         noPrimaryResource: false,
         sArtisan: true,
     });

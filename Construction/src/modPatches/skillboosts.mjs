93cc1b755c651e209ebe99960986fb8715f9d73c
Vladulentor <mihailvladutpopescu@gmail.com>
2025-10-02 15:28:49 +0300

    Made the progress pet more progress-y

--- Construction/src/modPatches/skillboosts.mjs
+++ Construction/src/modPatches/skillboosts.mjs
@@ -1,11 +1,10 @@
 export function skillBoostsCompatibility(ctx) {
-    let myheader= document.getElementById('rielk-mastery');
     skillBoosts.addNewSkill({
         // Required //
         skill: game.construction,
         // Optional //
         realmIDs: ['melvorD:Melvor'], // My god Slash, you really made it easy for idiots like me, after trying to get ETA to somehow work for a day straight this is amazing
-        header: myheader, // A header is only required if there is no `skill.header` property //
+        header: '#skill-boosts-append', // second thanks for this <-
         noPreservation: false,
         noMastery: true,
         noSummon: true,
@@ -15,6 +14,23 @@ export function skillBoostsCompatibility(ctx) {
         noConsumable: true,
         noPrimaryResource: false,
         isArtisan: true,
+    });
+
+    ctx.patch(skillBoosts.__proto__.constructor, 'createPetTooltip').after(function (_, container, item) {
+        if (item.id === "rielkConstruction:Scoobs") {
+            
+
+            let progress = game.construction.recipeCountByTier.reduce((a, b) => a + b, 0);
+            let goal = game.construction.recipeNumber * game.construction.tierMasteries.size;
+            const miscContainer = container.querySelector('.sb-font-sm');
+            miscContainer.querySelectorAll('.sb-font-2sm').forEach(el => el.remove());
+            const elem = document.createElement('span');
+            elem.className = 'text-info sb-font-2sm';
+            elem.style.marginTop = '4px';
+            elem.textContent = `${getLangString('TUTORIAL_MISC_0')}: ${numberWithCommas(progress)}/${numberWithCommas(goal)}`;
+
+            miscContainer.appendChild(elem);
+        }
     });
 
 }   
